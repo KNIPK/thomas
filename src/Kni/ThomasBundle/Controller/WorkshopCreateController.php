21a878c301b4ad839b4ba93620e8734dbe1d9aae
jaworekmichal <jaworekmichal@gmail.com>
2013-04-07 11:57:32 +0100

    View for StepsController - firts prototype

--- src/Kni/ThomasBundle/Controller/WorkshopCreateController.php
+++ src/Kni/ThomasBundle/Controller/WorkshopCreateController.php
@@ -10,17 +10,25 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  * @Route("/profile/workshopcreate")
  */
 class WorkshopCreateController extends Controller {
+
     /**
      * @Route("/step")
      * @Template()
      */
-    public function stepAction(){
-        
+    public function stepAction() {
+        //it is responsible for creating one particular step (it's part of procces of creating steps (stepsAction))    
         return array();
     }
-    
+
+    /**
+     * @Route("/steps")
+     * @Template()
+     */
+    public function stepsAction() {
+        //add steps and questions
+        return array();
+    }
+
 }
-
-
 
 ?>

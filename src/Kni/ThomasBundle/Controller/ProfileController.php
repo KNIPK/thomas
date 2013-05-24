447a977b2bc7cf8a7a16280635594d78a724f0b2
jaworekmichal <jaworekmichal@gmail.com>
2013-05-24 23:15:15 +0200

    Prawie dziala edycja profilu (brakuje mi jednej linijki)

--- src/Kni/ThomasBundle/Controller/ProfileController.php
+++ src/Kni/ThomasBundle/Controller/ProfileController.php
@@ -2,32 +2,33 @@
 
 namespace Kni\ThomasBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Kni\ThomasBundle\DependencyInjection\NavigationBar;
-
+use Kni\ThomasBundle\Form\Type\UserEditType;
+use Kni\ThomasBundle\Form\Type\UserChangePassType;
 
 /**
- *Profile controller.
+ * Profile controller.
  *
  * @Route("/profile")
  */
-class ProfileController extends Controller
-{
-    function __construct(){
+class ProfileController extends Controller {
+
+    function __construct() {
         NavigationBar::add("Profil", "profile");
     }
-    
+
     /**
      * @Route("/index", name="profile")
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         return $this->render('KniThomasBundle:Profile:index.html.twig', array());
     }
-    
+
     /**
      * Finds and displays a Profile entity.
      *
@@ -35,8 +36,7 @@ class ProfileController extends Controller
      * @Method("GET")
      * @Template()
      */
-    public function showAction()
-    {
+    public function showAction() {
         $em = $this->getDoctrine()->getManager();
         $user = $this->get('security.context')->getToken()->getUser();
         $entity = $em->getRepository('KniThomasBundle:User')->find($user->getId());
@@ -45,10 +45,84 @@ class ProfileController extends Controller
             throw $this->createNotFoundException('Unable to find User entity.');
         }
 
+
         return array(
-            'entity'      => $entity
+            'entity' => $entity
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing profile entity.
+     *
+     * @Route("/edit", name="profile_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction() {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->get('security.context')->getToken()->getUser();
+        $entity = $em->getRepository('KniThomasBundle:User')->find($user->getId());
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Workshop entity.');
+        }
+
+
+        $editForm = $this->createForm(new UserEditType(), $entity);
+        $passForm = $this->createForm(new UserChangePassType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'pass_form' => $passForm->createView(),
+            'info' => ''
         );
     }
     
     
+     /**
+     * Edits users name and surname.
+     *
+     * @Route("/changeName", name="user_name_update")
+     * @Method("PUT")
+     * @Template("KniThomasBundle:Profile:edit.html.twig")
+     */
+    public function updateAction(Request $request)
+    {
+        $id = $this->get('security.context')->getToken()->getUser()->getId();
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository('KniThomasBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Workshop entity.');
+        }
+
+        
+        $editForm = $this->createForm(new UserEditType(), $entity);
+        $passForm = $this->createForm(new UserChangePassType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $entity->setName('spytacMateusza');
+            $entity->setSurname('spytacMateusza');
+            $em->persist($entity);
+            $em->flush();
+
+                    return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'pass_form' => $passForm->createView(),
+            'info' => 'Zmiana dokonana poprawnie');
+        } else{
+           return array(
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'pass_form' => $passForm->createView(),
+            'info' => 'Nie udało się wprowadzić zmian');
+        }
+
+
+    }
+    
+    
+
 }

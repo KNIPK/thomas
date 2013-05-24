6d96a434d9d1bab3947a44ce5c02635c8e7197f0
jaworekmichal <jaworekmichal@gmail.com>
2013-05-24 21:31:09 +0200

    Dodanie metody i widoku show profile. (nie wiem czemu oznaczyło mi, że zmieniłem wszystkie pliki)

--- src/Kni/ThomasBundle/Controller/ProfileController.php
+++ src/Kni/ThomasBundle/Controller/ProfileController.php
@@ -3,10 +3,17 @@
 namespace Kni\ThomasBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Kni\ThomasBundle\DependencyInjection\NavigationBar;
 
+
+/**
+ *Profile controller.
+ *
+ * @Route("/profile")
+ */
 class ProfileController extends Controller
 {
     function __construct(){
@@ -14,11 +21,34 @@ class ProfileController extends Controller
     }
     
     /**
-     * @Route("/profile/index", name="profile")
+     * @Route("/index", name="profile")
      */
     public function indexAction()
     {
         return $this->render('KniThomasBundle:Profile:index.html.twig', array());
     }
+    
+    /**
+     * Finds and displays a Profile entity.
+     *
+     * @Route("/show", name="profile_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->get('security.context')->getToken()->getUser();
+        $entity = $em->getRepository('KniThomasBundle:User')->find($user->getId());
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find User entity.');
+        }
+
+        return array(
+            'entity'      => $entity
+        );
+    }
+    
     
 }

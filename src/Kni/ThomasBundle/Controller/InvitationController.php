5b4fa4dc6347262ac2ee5f937ab5d82e7242862c
jaworekmichal <jaworekmichal@gmail.com>
2013-06-10 17:55:29 +0200

    wysyłanie maili z zaproszeniami

--- src/Kni/ThomasBundle/Controller/InvitationController.php
+++ src/Kni/ThomasBundle/Controller/InvitationController.php
@@ -16,6 +16,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class InvitationController extends Controller {
 
     /**
+     * Send invitations.
+     *
+     * @Route("/send/", name="send_invitations")
+     * @Method("POST")
+     * @Template("KniThomasBundle:Workshop:added.html.twig")
+     */
+    public function sendMailsAction(Request $request) {
+
+        $data = $request->request->all();
+        $mails = explode(',', $data['mails'], 100);
+        $text = $data['text'];
+        foreach ($mails as $mail) {
+            $message = \Swift_Message::newInstance()->setSubject('Zaproszenie do warsztatu: '.$data['name'])->setFrom('thomas@kalinowski.net.pl')
+                            ->setTo($mail)->setBody($data['text']);
+            $this->get('mailer')->send($message);
+        }
+
+        return array();
+    }
+
+    /**
      * Creates a new Workshop entity.
      *
      * @Route("/{workshopId}/", name="workshop_invitation")

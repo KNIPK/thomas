2d764306869232d11afd42a9c73210de80cf1608
jaworekmichal <jaworekmichal@gmail.com>
2013-06-10 18:05:33 +0200

    obcinanie spacji w mailu

--- src/Kni/ThomasBundle/Controller/InvitationController.php
+++ src/Kni/ThomasBundle/Controller/InvitationController.php
@@ -29,7 +29,7 @@ class InvitationController extends Controller {
         $text = $data['text'];
         foreach ($mails as $mail) {
             $message = \Swift_Message::newInstance()->setSubject('Zaproszenie do warsztatu: '.$data['name'])->setFrom('thomas@kalinowski.net.pl')
-                            ->setTo($mail)->setBody($data['text']);
+                            ->setTo(trim($mail))->setBody($data['text']);
             $this->get('mailer')->send($message);
         }
 
